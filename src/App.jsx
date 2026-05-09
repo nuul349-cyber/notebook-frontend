a30e8983631ac41082b91fc51feb7f3f6d3bd7d8
Nuul <nuul@example.com>
2026-05-09 15:12:52 -0600

    Add login frontend and Fix connection with the backend.

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
-  
+  const [username, setUsername] = useState('')
+  const {password, setPassword} = useState('')
+
   useEffect(() => {
     noteService
       .getAll()
@@ -61,10 +63,41 @@ const App = () => {
       })
   }
 
+  const handleLogin = event => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
+
   return (
     <div>
       <h1>Notes app</h1>
       <Notification message={errorMessage} />
+
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            username
+            <input
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
