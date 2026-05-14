4c72877003fff72a94cac4c9413dd63745e257ad
Nuul <nuul@example.com>
2026-05-14 11:13:58 -0600

    Add userEvent test

--- src/components/Note.test.jsx
+++ src/components/Note.test.jsx
@@ -1,14 +1,22 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
-import { expect } from 'vitest'
+import { expect, vi } from 'vitest'
 
-test('renders content', () => {
+test('renders content', async () => {
   const note = {
     content: 'Component testing is done with react-testing-library',
     important: true
   }
-  render(<Note note={note} />)
 
-  const element = screen.getByText('Component testing is done with react-testing-library')
-  expect(element).toBeDefined()
+  const mockHandler = vi.fn()
+  render(
+    <Note note={note} toggleImportance={mockHandler} />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
